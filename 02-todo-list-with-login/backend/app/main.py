aa1a1edd69c9508b297b7f873ae1e5fbf6f02697
ron <lucifron28@gmail.com>
2025-06-24 01:04:56 +0800

    add auth routes

--- 02-todo-list-with-login/backend/app/main.py
+++ 02-todo-list-with-login/backend/app/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from app import models, database
-# from app.routes import auth, todos
+from app.routes import auth as auth_router
 
 models.Base.metadata.create_all(bind=database.engine)
 
@@ -15,5 +15,5 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-# app.include_router(auth.router, prefix="/auth", tags=["Auth"])
+app.include_router(auth_router.router, prefix="/auth", tags=["Auth"])
 # app.include_router(todos.router, prefix="/todos", tags=["Todos"])
