b855383239be1efd8926ce5595a07589d788631f
ron <lucifron28@gmail.com>
2025-06-24 00:51:49 +0800

    create the functions

---
+++ 02-todo-list-with-login/backend/app/auth.py
@@ -0,0 +1,31 @@
+from jose import jwt, JWTError
+from .models import User
+from datetime import datetime, timedelta, timezone
+
+SECRET_KEY = "not-really-a-secret"
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
+REFRESH_TOKEN_EXPIRE_DAYS = 7
+
+def create_access_token(data: dict, expires_delta: timedelta=None) -> str:
+    to_encode = data.copy()
+    expire = datetime.now(timezone.utc) + (expires_delta or ACCESS_TOKEN_EXPIRE_MINUTES)
+    to_encode = data.update({"exp": expire, "type": "access"})
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
+
+def create_refresh_token(data: dict, expires: timedelta=None) -> str:
+    to_encode = data.copy()
+    expire = datetime.now(timezone.utc)
+    to_encode = data.update({"exp": expire, "type": "refresh"})
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
+
+def verify_token(token: str, token_type: str = "access"):
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        if payload.get("type:") != token_type:
+            return None
+        return payload
+    except JWTError:
+        return None
