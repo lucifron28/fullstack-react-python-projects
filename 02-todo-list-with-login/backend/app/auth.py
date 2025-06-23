d32ee6a287fe9ac0ff60e8a896c853933555add7
ron <lucifron28@gmail.com>
2025-06-24 00:55:00 +0800

    fix typos

--- 02-todo-list-with-login/backend/app/auth.py
+++ 02-todo-list-with-login/backend/app/auth.py
@@ -7,24 +7,24 @@ ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 30
 REFRESH_TOKEN_EXPIRE_DAYS = 7
 
-def create_access_token(data: dict, expires_delta: timedelta=None) -> str:
+def create_access_token(data: dict, expires_delta: timedelta = None) -> str:
     to_encode = data.copy()
-    expire = datetime.now(timezone.utc) + (expires_delta or ACCESS_TOKEN_EXPIRE_MINUTES)
-    to_encode = data.update({"exp": expire, "type": "access"})
+    expire = datetime.now(timezone.utc) + (expires_delta or timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES))
+    to_encode.update({"exp": expire, "type": "access"})
     encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
     return encoded_jwt
 
-def create_refresh_token(data: dict, expires: timedelta=None) -> str:
+def create_refresh_token(data: dict, expires_delta: timedelta = None) -> str:
     to_encode = data.copy()
-    expire = datetime.now(timezone.utc)
-    to_encode = data.update({"exp": expire, "type": "refresh"})
+    expire = datetime.now(timezone.utc) + (expires_delta or timedelta(days=REFRESH_TOKEN_EXPIRE_DAYS))
+    to_encode.update({"exp": expire, "type": "refresh"})
     encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
     return encoded_jwt
 
 def verify_token(token: str, token_type: str = "access"):
     try:
         payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
-        if payload.get("type:") != token_type:
+        if payload.get("type") != token_type:
             return None
         return payload
     except JWTError:
